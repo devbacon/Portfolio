4ef250224adcac19764dbbfdd36fd841459f7f00
Devon Bacon <metalogicoder@users.noreply.github.com>
2018-09-25 12:30:28 -0700

    Header.js - completed base version of Header component

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -7,9 +7,31 @@ class Header extends Component {
     super();
 
     this.nav = React.createRef();
+    this.navItems = React.createRef();
+    this.hamburger = React.createRef();
 
     this.state = {
       collapsed: true
+    }
+  }
+
+  toggleNav = () => {
+    this.setState({ collapsed: !this.state.collapsed });
+    const delay = 250
+
+    // Use delay for smooth animation
+    if (this.state.collapsed) {
+      this.navItems.current.style.top = '65px';
+      this.hamburger.current.classList.toggle('change');
+      setTimeout(() => {
+        this.hamburger.current.classList.toggle('cross');
+      }, delay);
+    } else {
+      this.navItems.current.style.top = '-65px';
+      this.hamburger.current.classList.toggle('cross');
+      setTimeout(() => {
+        this.hamburger.current.classList.toggle('change');
+      }, delay);
     }
   }
   
@@ -18,14 +40,14 @@ class Header extends Component {
       <div className="header-container">
         <h1 className="header-title">{this.props.title}</h1>
         <div className="header-nav">
-          <div className="nav-hamburger" onClick={this.toggleNav}>
+          <div onClick={this.toggleNav} ref={this.hamburger}>
             <div className="bar1" />
             <div className="bar2" />
             <div className="bar3" />
 				  </div>
-          <div className="nav-items">
+          <div className="nav-items" ref={this.navItems}>
             {this.state.collapsed ? null : this.props.navItems.map(item => (
-              <Link to={item === 'Home' ? '' : item.toLowerCase()}>
+              <Link to={item === 'Home' ? '' : item.toLowerCase()} key={item}>
                 <h2 className="header-nav-item">{item}</h2>
               </Link>
             ))}
