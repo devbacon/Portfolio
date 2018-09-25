4e7aef20d8c1d782b62fe33802800b8d2496fbe0
Devon Bacon <metalogicoder@users.noreply.github.com>
2018-09-24 19:34:43 -0700

    App.js - added switch paths and imported components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
 import Landing from './Landing/Landing.js';
+import About from './About/About.js';
+import Contact from './Contact/Contact.js';
+import Projects from './Projects/Projects.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Landing />
-      </div>
+      <Switch>
+        <Route exact path='/' component={Landing} />
+        <Route path='/about' component={About} />
+        <Route path='/contact' component={Contact} />
+        <Route path='/projects' component={Projects} />
+      </Switch>
     );
   }
 }
