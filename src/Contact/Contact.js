ec504f7a79e3e8b1b523c9592176d4cf82d89a1c
Devon Bacon <metalogicoder@users.noreply.github.com>
2018-09-26 15:23:18 -0700

    Contact.js - added overlay ref and placeholder content

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -3,13 +3,25 @@ import Header from '../Header/Header.js';
 import './Contact.css';
 
 class Contact extends Component {
+  constructor() {
+    super();
+
+    this.overlay = React.createRef();
+  }
+
   render() {
     return (
       <div className="page-container">
-        <Header title="Contact" navItems={['Home', 'About', 'Projects']} history={this.props.history}/>
+        <Header 
+          title="Contact"
+          navItems={['Home', 'About', 'Projects']}
+          overlay={this.overlay}
+          history={this.props.history}
+        />
         <div className="page-body">
-
+          <h1>TESTING!!!</h1>
         </div>
+        <div className="page-overlay" ref={this.overlay}></div>
       </div>
     );
   }
