9b35ef88963ff15703704dc1e2aad79b0ba5f758
Devon Bacon <metalogicoder@users.noreply.github.com>
2018-09-26 15:23:29 -0700

    Projects.js - added overlay ref and placeholder content

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -3,13 +3,25 @@ import Header from '../Header/Header.js';
 import './Projects.css';
 
 class Projects extends Component {
+  constructor() {
+    super();
+
+    this.overlay = React.createRef();
+  }
+
   render() {
     return (
       <div className="page-container">
-        <Header title="Projects" navItems={["Home", "About", "Contact"]} history={this.props.history}/>
+        <Header 
+          title="Projects" 
+          navItems={["Home", "About", "Contact"]} 
+          overlay={this.overlay}
+          history={this.props.history}
+        />
         <div className="page-body">
-
+         <h1>TESTING!!!</h1>
         </div>
+        <div className="page-overlay" ref={this.overlay}></div>
       </div>
     );
   }
