f6c66d90a5154e612d30b030dda105a7e2853bf6
Devon Bacon <metalogicoder@users.noreply.github.com>
2018-09-26 15:22:32 -0700

    About.js - added overlay ref and basic skeleton for content

--- src/About/About.js
+++ src/About/About.js
@@ -3,13 +3,30 @@ import Header from '../Header/Header.js';
 import './About.css';
 
 class About extends Component {
+  constructor() {
+    super();
+    
+    this.overlay = React.createRef();
+  }
+
   render() {
     return (
       <div className="page-container">
-        <Header title="About" navItems={['Home', 'Contact', 'Projects']} history={this.props.history} />
-        <div className="page-body">
-
+        <Header 
+          title="About" 
+          navItems={['Home', 'Contact', 'Projects']} 
+          overlay={this.overlay}
+          history={this.props.history}
+        />
+        <div className="page-body" ref={this.pageBody}>
+          <div className="about-col-left">
+            <p></p>
+          </div>
+          <div className="about-col-right">
+            <p></p>
+          </div>
         </div>
+        <div className="page-overlay" ref={this.overlay}></div>
       </div>
     );
   }
