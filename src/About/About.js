a48ead5a7003d668cbcce1e6fb12e822d56d8255
Devon Bacon <metalogicoder@users.noreply.github.com>
2018-10-02 14:08:41 -0700

    About.js - more about text content edits

--- src/About/About.js
+++ src/About/About.js
@@ -26,20 +26,13 @@ class About extends Component {
 						<img src={AboutImage} alt="devon bacon" />
 					</div>
 					<p>
-						Hello. My name is Devon Bacon. I am a programmer with a passion for web development living in
-						the San Francisco Bay area.
+						Hello, my name is Devon Bacon. I am a programmer with a passion for web development living in the San Francisco Bay area.
 					</p>
 					<p>
-						Before becoming a programmer my primary professions were freeway traffic control, underground
-						utility identification, and dog training. All of these occupations relied heavily on
-						communication and problem-solving in very different ways.
+						Before becoming a programmer my primary professions were freeway traffic control, underground utility identification, and dog training. All of these occupations relied heavily on communication and problem-solving in very different ways.
 					</p>
 					<p>
-						Lambda School helped me to adapt my previous skills to the world of software development. During
-						my six months there, I was taught the fundamentals of computer science and built
-						applications using countless technologies. The priority was being able to learn quickly while
-						tackling problems that you have never faced before. I came into this program with a strong
-						foundation in Javascript and left knowing that I could pick up any other technology with ease.
+						Lambda School is the computer science academy that helped me to adapt my previous skills to the world of software development. During my six months there, I was taught the fundamentals of computer science and built applications using countless technologies. The priority was being able to learn quickly while tackling problems that you have never faced before. I came into this program with a strong foundation in Javascript and left knowing that I could pick up any other technology with ease.
 					</p>
 				</div>
 				<div className="page-overlay" ref={this.overlay} />
