39abc0cb8b10d97303d7966e6143878200d2a1b0
Devon Bacon <metalogicoder@users.noreply.github.com>
2018-10-01 22:19:31 -0700

    Header.js - refactored element order to acoomodate new fixed header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,99 +2,100 @@ import React, { Component } from 'react';
 import './Header.css';
 
 class Header extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.header = React.createRef();
-    this.nav = React.createRef();
-    this.navItems = React.createRef();
-    this.hamburger = React.createRef();
+		this.header = React.createRef();
+		this.nav = React.createRef();
+		this.navItems = React.createRef();
+		this.hamburger = React.createRef();
 
-    this.state = {
-      collapsed: true
-    }
-  }
+		this.state = {
+			collapsed: true
+		};
+	}
 
-  componentDidMount() {
-    setTimeout(() => { 
-      this.header.current.style.marginTop = 0;
-    }, 100);
+	componentDidMount() {
+		setTimeout(() => {
+			this.header.current.style.marginTop = 0;
+		}, 100);
 
-    setTimeout(() => {
-      if (this.props.overlay.current) {
-        this.props.overlay.current.style.background = 'none';
-      }
-    }, 500);
+		setTimeout(() => {
+			if (this.props.overlay.current) {
+				this.props.overlay.current.style.background = 'none';
+			}
+		}, 500);
 
-    setTimeout(() => {
-      if (this.props.overlay.current) {
-        this.props.overlay.current.style.visibility = 'hidden';
-      } 
-    }, 1200);
-  }
+		setTimeout(() => {
+			if (this.props.overlay.current) {
+				this.props.overlay.current.style.visibility = 'hidden';
+			}
+		}, 1200);
+	}
 
-  goTo = (path) => {
-    this.toggleNav();
-    this.props.overlay.current.style.visibility = 'visible';
-    this.props.overlay.current.style.background = '#383838';
+	goTo = (path) => {
+		this.toggleNav();
+		this.props.overlay.current.style.visibility = 'visible';
+		this.props.overlay.current.style.background = '#383838';
 
-    setTimeout(() => {
-      this.header.current.style.marginTop = '-100px';
-    }, 100);
-    
-    setTimeout(() => {
-      this.props.history.push(`/${path}`);
-    }, 500);
-  }
+		setTimeout(() => {
+			this.header.current.style.marginTop = '-100px';
+		}, 100);
 
-  toggleNav = () => {
-    this.setState({ collapsed: !this.state.collapsed });
-    const delay = 250
-    const navStyle = this.navItems.current.style;
-    const hamClass = this.hamburger.current.classList;
+		setTimeout(() => {
+			this.props.history.push(`/${path}`);
+		}, 500);
+	};
 
-    // Use delay for smooth animation
-    if (this.state.collapsed) {
-      navStyle.visibility = 'visible';
-      navStyle.top = '65px';
-      hamClass.toggle('change');
+	toggleNav = () => {
+		this.setState({ collapsed: !this.state.collapsed });
+		const delay = 250;
+		const navStyle = this.navItems.current.style;
+		const hamClass = this.hamburger.current.classList;
 
-      setTimeout(() => {
-        hamClass.toggle('cross');
-      }, delay);
-    } else {
-      navStyle.visibility = 'hidden';
-      navStyle.top = '-110px';
-      hamClass.toggle('cross');
+		// Use delay for smooth animation
+		if (this.state.collapsed) {
+			navStyle.visibility = 'visible';
+			navStyle.top = '65px';
+			hamClass.toggle('change');
 
-      setTimeout(() => {
-        hamClass.toggle('change');
-      }, delay);
-    }
-  }
-  
-  render() {
-    return (
-      <div className="header-container" ref={this.header}>
-        <h1 className="header-title">{this.props.title}</h1>
-        <div className="header-nav">
-          <div className="hamburger" onClick={this.toggleNav} ref={this.hamburger}>
-            <div className="bar1" />
-            <div className="bar2" />
-            <div className="bar3" />
-				  </div>
-          <div className="nav-items" ref={this.navItems}>
-            {this.props.navItems.map(item => (
-              <div key={item} onClick={() => this.goTo(item === 'Home' ? '' : item.toLowerCase())}>
-                <h2 className="header-nav-item">{item}</h2>
-              </div>
-            ))}
-          </div>
-      
-        </div>
-      </div>
-    );
-  }
+			setTimeout(() => {
+				hamClass.toggle('cross');
+			}, delay);
+		} else {
+			navStyle.visibility = 'hidden';
+			navStyle.top = '-110px';
+			hamClass.toggle('cross');
+
+			setTimeout(() => {
+				hamClass.toggle('change');
+			}, delay);
+		}
+	};
+
+	render() {
+		return (
+			<div>
+				<div className="header-container" ref={this.header}>
+					<h1 className="header-title">{this.props.title}</h1>
+					<div className="header-nav">
+						<div className="hamburger" onClick={this.toggleNav} ref={this.hamburger}>
+							<div className="bar1" />
+							<div className="bar2" />
+							<div className="bar3" />
+						</div>
+					</div>
+				</div>
+				<div className="nav-items" ref={this.navItems}>
+					{this.props.navItems.map((item) => (
+						<div key={item} onClick={() => this.goTo(item === 'Home' ? '' : item.toLowerCase())}>
+							<h2 className="header-nav-item">{item}</h2>
+						</div>
+					))}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Header;
