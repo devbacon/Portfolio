4ddf060a4830fdb1debdec029017e9830aabe6e2
Devon Bacon <metalogicoder@users.noreply.github.com>
2018-10-01 20:01:50 -0700

    ProjectCard.js - added github and site links

--- src/Projects/ProjectCard.js
+++ src/Projects/ProjectCard.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 
+import ExternalLinkIcon from '../media/images/external-link-icon.png';
+import GitHubIcon from '../media/images/github-icon.png';
+
 class ProjectCard extends Component {
   constructor() {
     super();
+
+    this.cardLinks = React.createRef();
 
     this.tabs = {
       summary: React.createRef(),
@@ -64,12 +69,28 @@ class ProjectCard extends Component {
           <div className="details-display">{this.props[this.state.tab]}</div>
         </div>
         <div className="card-image">
-          <a href={this.props.link} target="_blank" rel="noopener noreferrer">
-            <img 
-              src={this.props.image} 
-              alt="Project Thumbnail"
+          <img 
+            className="card-image-preview"
+            src={this.props.image} 
+            alt="project preview"
+          />
+          <div className="card-link-container">
+          <a href={this.props.github} target="_blank" rel="noopener noreferrer">
+            <img
+              className="card-link card-link-github" 
+              src={GitHubIcon} 
+              alt="external link icon" 
             />
           </a>
+          <a href={this.props.link} target="_blank" rel="noopener noreferrer">
+            <img
+              className="card-link card-link-external" 
+              src={ExternalLinkIcon} 
+              alt="external link icon" 
+            />
+          </a>
+        
+          </div>
         </div>
       </div>
     );
