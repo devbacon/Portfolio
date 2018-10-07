ea77a5f378a7b0bd38f7166b8348a382462f2e14
Devon Bacon <15711397+metalogicoder@users.noreply.github.com>
2018-10-06 18:50:26 -1000

    Header.js - removed old JSX

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -73,17 +73,7 @@ class Header extends Component {
 						</div>
 					</div>
 				</div>
-				{/* <div className="nav-items" ref={this.navItems}>
-					{this.props.navItems.map((item) => (
-            <div key={item}>
-              <div onClick={() => this.goTo(item === 'Home' ? '' : item.toLowerCase())}>
-                <h2 className="header-nav-item">{item}</h2>
-              </div>
-              { item !== this.props.navItems[this.props.navItems.length - 1] ?
-                <div className="nav-items-divide"></div> : null }
-            </div>
-					))}
-				</div> */}
+				
 			</div>
 		);
 	}
