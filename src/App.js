4bab1af9bca74b02c9e1f1fbcbd79947141048b7
Devon Bacon <metalogicoder@users.noreply.github.com>
2018-10-24 17:06:32 -0700

    App.js - formats document with prettier standards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom';
 import Landing from './Landing/Landing.js';
 import About from './About/About.js';
 import Contact from './Contact/Contact.js';
@@ -7,16 +7,16 @@ import Projects from './Projects/Projects.js';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <Switch>
-        <Route exact path='/' component={Landing} />
-        <Route path='/about' component={About} />
-        <Route path='/contact' component={Contact} />
-        <Route path='/projects' component={Projects} />
-      </Switch>
-    );
-  }
+	render() {
+		return (
+			<Switch>
+				<Route exact path="/" component={Landing} />
+				<Route path="/about" component={About} />
+				<Route path="/contact" component={Contact} />
+				<Route path="/projects" component={Projects} />
+			</Switch>
+		);
+	}
 }
 
 export default App;
